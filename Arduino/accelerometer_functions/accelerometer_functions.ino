9fee05ac52415daf1cefa59cabca4e1cccde041a
Megan Johnson <megan.ann.johnson7@gmail.com>
2020-04-10 14:24:41 -0700

    Add accelerometer hello world with basic stepcount functionality

---
+++ Arduino/accelerometer_functions/accelerometer_functions.ino
@@ -0,0 +1,52 @@
+#include "Wire.h" 
+const int MPU_ADDR = 0x68; 
+int16_t accelerometer_x, accelerometer_y, accelerometer_z; 
+int16_t gyro_x, gyro_y, gyro_z; 
+int16_t temperature; 
+char tmp_str[7]; 
+char* convert_int16_to_str(int16_t i) {
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
+}
+int paceflag = 0; //0 says that since the last negative value, there has not been a value of 2000+. One means since the last 2000+, there has not been a negative value.
+int stepcount = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
+  Wire.write(0x6B); // PWR_MGMT_1 register
+  Wire.write(0); // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+}
+void loop() {
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
+  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+  Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
+  
+  // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
+  accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+  accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+  accelerometer_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
+  temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
+  gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+  gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+  gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+  
+  if (paceflag == 0){
+    if (gyro_y > 2000){
+      stepcount = stepcount +1;
+      paceflag = 1;
+      Serial.print("Stepcount: "); Serial.print(stepcount, DEC);
+    }
+  }
+  else if (paceflag == 1){  
+    if (gyro_y < 0){
+      paceflag = 0;
+    }
+  }
+  //Serial.print(" | gY = "); Serial.print(convert_int16_to_str(gyro_y));
+  
+  delay(10);
+}
