8afefe79a72c410c27fc06479aaa1961e5837353
Megan Johnson <megan.ann.johnson7@gmail.com>
2020-04-10 14:47:13 -0700

    Add skeleton code for ESPNow communication, improve annotation

---
+++ Arduino/ESPNOW_Code/master/master.ino
@@ -0,0 +1,141 @@
+// Megan Johnson
+// Date: 4/10/2020
+// Code for Master ESP8266 to be integrated into wearable for BME 310 Final Project
+
+//--------------------------Include Statements---------------------------/
+
+#include <Wire.h>
+//#include "MAX30105.h"
+#include <ESP8266WiFi.h>
+extern "C" {
+    #include <espnow.h>
+}
+
+//--------------------------Wireless Communication Setup---------------------------//
+// this is the MAC Address of the slave which receives the data
+uint8_t remoteMac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x34};
+
+#define WIFI_CHANNEL 4
+
+
+//--------------------------Packet Definition--------------------------------------//
+// must match the slave struct
+struct __attribute__((packed)) DataStruct {
+    //long pulse;
+    long gyro_y
+};
+
+DataStruct myData;
+
+
+//--------------------------Variable Initilaization---------------------------------//
+//General Variables
+unsigned long lastSentMillis;
+unsigned long sendIntervalMillis = 100;
+unsigned long sentMicros;
+unsigned long ackMicros;
+ 
+unsigned long lastBlinkMillis;
+unsigned long fastBlinkMillis = 200;
+unsigned long slowBlinkMillis = 700;
+unsigned long blinkIntervalMillis = slowBlinkMillis;
+
+//MAX 30105 (PulseOx)
+//MAX30105 particleSensor; 
+byte ledPin = 14;
+
+//MPU 6050 (Accelerometer)
+
+
+//==============
+
+void setup() {
+
+//-----------------------Wireless Initiliazation--------------------------------------//
+    Serial.begin(115200); Serial.println();
+    Serial.println("Starting EspnowController.ino");
+
+    WiFi.mode(WIFI_STA); // Station mode for esp-now controller
+    WiFi.disconnect();
+
+    if (esp_now_init() != 0) {
+        Serial.println("*** ESP_Now init failed");
+        while(true) {};
+    }
+    esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+    esp_now_add_peer(remoteMac, ESP_NOW_ROLE_SLAVE, WIFI_CHANNEL, NULL, 0);
+
+    esp_now_register_send_cb(sendCallBackFunction);
+
+//--------------------------MAX 30105 Initialization------------------------------------//
+  // Initialize MAX 30105 Sensor
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+     Serial.println("MAX30105 was not found. Please check wiring/power. ");
+     while (1);
+  }
+
+  //Setup to sense a nice looking saw tooth on the plotter
+   byte ledBrightness = 0xF0; //Options: 0=Off to 255=50mA
+   byte sampleAverage = 8; //Options: 1, 2, 4, 8, 16, 32
+   byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+   int sampleRate = 1000; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+   int pulseWidth = 411; //Options: 69, 118, 215, 411
+   int adcRange = 16384; //Options: 2048, 4096, 8192, 16384
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+
+//-------------------------MPU 6050 Initialization--------------------------------------//
+
+
+}
+
+
+//==============
+
+void loop() {
+    if (particleSensor.getIR() < 8000) {
+        //Serial.println("SLeeping");
+        ESP.deepSleep(1e6);
+
+    }
+    else{
+        //Serial.println("Waking up");
+        sendData();
+        //Serial.println(particleSensor.getIR()); //Send raw data to plotter
+    }
+}
+
+//==============
+
+void sendData() {
+    if (millis() - lastSentMillis >= sendIntervalMillis) {
+        lastSentMillis += sendIntervalMillis;
+        long TempIr = particleSensor.getIR();
+        if ((TempIr < 1e9) && (TempIr > -1e9)){
+            myData.Pulse = TempIr;
+        } 
+        uint8_t bs[sizeof(myData)];
+        memcpy(bs, &myData, sizeof(myData));
+        sentMicros = micros();
+        esp_now_send(NULL, bs, sizeof(myData)); // NULL means send to all peers
+        Serial.println(myData.pulse);
+        //Serial.println("sent data");
+
+    }
+}
+
+//==============
+
+void sendCallBackFunction(uint8_t* mac, uint8_t sendStatus) {
+    ackMicros = micros();
+    Serial.println();
+    if (sendStatus == 0) {
+        blinkIntervalMillis = fastBlinkMillis;
+    }
+    else {
+        blinkIntervalMillis = slowBlinkMillis;
+    }
+}
+
+//================
---
+++ Arduino/ESPNOW_Code/slave/slave.ino
@@ -0,0 +1,67 @@
+// EspnowSlave.ino
+
+// a minimal program derived from
+//          https://github.com/HarringayMakerSpace/ESP-Now
+
+// This is the program that receives the data. (The Slave)
+
+//-----------------------Include Statements------------------------//
+#include <ESP8266WiFi.h>
+extern "C" {
+    #include <espnow.h>
+     #include <user_interface.h>
+}
+
+//-----------------------Wireless Setup-----------------------------//
+uint8_t mac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x34};
+
+
+void initVariant() {
+  WiFi.mode(WIFI_AP);
+  wifi_set_macaddr(SOFTAP_IF, &mac[0]);
+}
+
+#define WIFI_CHANNEL 4
+
+
+//-----------------------Packet Setup--------------------------------//
+struct __attribute__((packed)) DataStruct {
+    long Pulse;
+};
+
+DataStruct myData;
+
+
+void setup() {
+    Serial.begin(115200); Serial.println();
+    Serial.println("Starting EspnowSlave.ino");
+
+    Serial.print("This node AP mac: "); Serial.println(WiFi.softAPmacAddress());
+    Serial.print("This node STA mac: "); Serial.println(WiFi.macAddress());
+
+    if (esp_now_init()!=0) {
+        Serial.println("*** ESP_Now init failed");
+        while(true) {};
+    }
+
+    esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
+
+    esp_now_register_recv_cb(receiveCallBackFunction);
+
+
+    Serial.println("End of setup - waiting for messages");
+}
+
+//============
+
+void loop() {
+
+}
+
+//============
+
+void receiveCallBackFunction(uint8_t *senderMac, uint8_t *incomingData, uint8_t len) {
+    memcpy(&myData, incomingData, sizeof(myData));
+    Serial.print(myData.pulse);
+    Serial.println();
+}
